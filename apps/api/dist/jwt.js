1822f7566c2da8866637854592a511c7140e7553
C-AB-prog <dima.d.chat.gpt@gmail.com>
2026-03-22 18:37:38 +0300

    Delete apps directory

--- apps/api/dist/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import { config } from "./config.js";
-export function signJwt(payload) {
-    return jwt.sign(payload, config.jwtSecret, { expiresIn: "30d" });
-}
-export function verifyJwt(token) {
-    return jwt.verify(token, config.jwtSecret);
-}
